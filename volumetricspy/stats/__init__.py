11ec91b8dfd76dbff0756c49875d4e88384d411f
Santiago Cuervo <scuervo91@gmail.com>
2022-01-27 10:21:39 -0500

    update sgs

--- volumetricspy/stats/__init__.py
+++ volumetricspy/stats/__init__.py
@@ -3,3 +3,4 @@ from .variograms  import Variogram, Spherical, Exponential, Gaussian
 from .transforms import to_normal, NScaler
 from .grid import Grid
 from .krigging import OrdinaryKrigging, IndicatorOridinaryKrigging, ordinary_krigging
+from .sgs import SequentialGaussianSimulation
