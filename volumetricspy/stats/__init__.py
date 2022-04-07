dec8023c07eb07887570d3eaf344039f13ace201
Santiago Cuervo <scuervo91@gmail.com>
2022-04-07 15:27:44 -0500

    update iwd

--- volumetricspy/stats/__init__.py
+++ volumetricspy/stats/__init__.py
@@ -2,5 +2,5 @@ from .points import Dot, CloudPoints
 from .variograms  import Variogram, Spherical, Exponential, Gaussian
 from .transforms import to_normal, NScaler
 from .grid import Grid
-from .krigging import OrdinaryKrigging, IndicatorOridinaryKrigging, ordinary_krigging
+from .krigging import OrdinaryKrigging, IndicatorOridinaryKrigging, ordinary_krigging, IWD, inverse_weighted_distance
 from .sgs import SequentialGaussianSimulation
